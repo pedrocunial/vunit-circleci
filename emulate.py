5c5a22a01a27f3cca23d04daeb6cde57777fc8c5
Luciano Pereira Soares <lpsoares@gmail.com>
2017-03-30 20:29:30 -0300

    errei diretorio do assembly, agora acertaod

--- emulate.py
+++ emulate.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def emulate():
-	subprocess.call(['java', '-jar', 'emulator/Elemulator.jar',"assembly/Abs2.hack","-i","emulator/ram_in.mif","-o","emulator/ram_out2.mif","-c","1000"])
+	subprocess.call(['java', '-jar', 'emulator/Elemulator.jar',"assembly/Abs2.hack","-i","assembly/ram_in.mif","-o","assembly/ram_out2.mif","-c","1000"])
 
 if __name__ == '__main__':
 	emulate()
